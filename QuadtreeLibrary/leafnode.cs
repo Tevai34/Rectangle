dc6a22afa6fb7b341c83672a3ad4b81c7d77fe88
Benjamin <benjaminstandfield@gmail.com>
2025-02-15 00:59:03 -0500

    chore: fixed typos

--- QuadtreeLibrary/leafnode.cs
+++ QuadtreeLibrary/leafnode.cs
@@ -45,7 +45,7 @@ public class LeafNode : Node
     /// </summary>
     /// <param name="x">[TODO:description]</param>
     /// <param name="y">[TODO:description]</param>
-    public override void Delete(int x, int y) //NOTE: you should modive this to take a Rectangle parameter insead.)
+    public override void Delete(int x, int y) //NOTE: you should modify this to take a Rectangle parameter instead.
     {
         var rect = Rectangles.FirstOrDefault(r => r.x == x && r.y == y);
         if (rect != null)
