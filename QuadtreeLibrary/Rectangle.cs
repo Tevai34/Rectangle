0c450e8f31064a582f2084aa198a5c986e179321
Benjamin <benjaminstandfield@gmail.com>
2025-02-15 00:22:46 -0500

    chore: revised Rectangle.cs and added documentation placeholders.

--- QuadtreeLibrary/Rectangle.cs
+++ QuadtreeLibrary/Rectangle.cs
@@ -1,17 +1,45 @@
 namespace Quadtree;
 
+/// <summary>
+/// [TODO:description]
+/// </summary>
 public class Rectangle
 {
-    public int X { get; set; }
-    public int Y { get; set; }
-    public int Length { get; set; }
-    public int Width { get; set; }
+    /// <summary>
+    /// [TODO:description]
+    /// </summary>
+    public int x { get; set; }
 
+    /// <summary>
+    /// [TODO:description]
+    /// </summary>
+    public int y { get; set; }
+
+    /// <summary>
+    /// [TODO:description]
+    /// </summary>
+    public int length { get; set; }
+
+    /// <summary>
+    /// [TODO:description]
+    /// </summary>
+    public int width { get; set; }
+
+    /// <summary>
+    /// [TODO:description]
+    /// </summary>
+    /// <param name="x">[TODO:description]</param>
+    /// <param name="y">[TODO:description]</param>
+    /// <param name="length">[TODO:description]</param>
+    /// <param name="width">[TODO:description]</param>
     public Rectangle(int x, int y, int length, int width)
     {
-        X = x;
-        Y = y;
-        Length = length;
-        Width = width;
+        // variables should begin with a lowercase letter.
+        // You can access variable of the class with the same 
+        // name as parameters using: this.<variable name>
+        this.x = x;
+        this.y = y;
+        this.length = length;
+        this.width = width;
     }
 }
