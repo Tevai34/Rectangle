a2fce01adeebd5ab343446b9d87bee1384865613
Benjamin <benjaminstandfield@gmail.com>
2025-02-15 01:09:20 -0500

    chore: added note to delete Node.Test.cs

--- QuadtreeLibrary.Tests/Node.Test.cs
+++ QuadtreeLibrary.Tests/Node.Test.cs
@@ -1,2 +1,7 @@
 namespace Quadtree.Tests;
 
+//NOTE: this file should be deleted. You cannot create abstract classes, only
+//classes that implement the abstract classes. In this case, you can not create
+//a Node, but you can create LeafNode or InternalNode. As a result, test those
+//two and not Node.
+
