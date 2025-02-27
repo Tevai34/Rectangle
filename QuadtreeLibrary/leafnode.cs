2edd5ab8c536cbae473e13db605c90e46afcc727
Tevai34 <117667970+Tevai34@users.noreply.github.com>
2025-02-27 14:07:30 -0500

    Update leafnode.cs

--- QuadtreeLibrary/leafnode.cs
+++ QuadtreeLibrary/leafnode.cs
@@ -1,148 +1,103 @@
 namespace Quadtree;
 
-/// <summary>
-/// [TODO:description]
-/// </summary>
+/// A class representing a leaf node in the quadtree. This node holds the rectangles
+/// and handles insertion, deletion, and updates until it splits into sub-nodes when the threshold is exceeded.
 public class LeafNode : Node
 {
-    /// <summary>
-    /// [TODO:description]
-    /// </summary>
-    /// <param name="xMin">[TODO:description]</param>
-    /// <param name="yMin">[TODO:description]</param>
-    /// <param name="xMax">[TODO:description]</param>
-    /// <param name="yMax">[TODO:description]</param>
-    public LeafNode(int xMin, int yMin, int xMax, int yMax) : base(xMin, yMin, xMax, yMax)
-    {
-        //NOTE: you'll need two properties:
-        //  - threshold: The amount of rectangles that a node should hold before it splits.
-        //  - data: a list/array of the rectangles contained by the node.
-    }
+    // Threshold for the number of rectangles before splitting
+    private int threshold; 
 
-    /// <summary>
-    /// [TODO:description]
-    /// </summary>
-    /// <param name="rect">[TODO:description]</param>
+ 
+    /// Initializes a new instance of the LeafNode class with a given space and threshold.
+    /// The minimum x-coordinate of the node's space.
+    /// The minimum y-coordinate of the node's space.
+    /// The maximum x-coordinate of the node's space.
+    /// The maximum y-coordinate of the node's space.
+  public LeafNode(int threshold, Rectangle space) 
+    : base(space.x, space.y, space.x + space.length, space.y + space.width) 
+{
+    this.threshold = threshold;
+    this.Rectangles = new List<Rectangle>();
+}
+
+
+
+    /// Inserts a rectangle into the node. If the node exceeds the threshold, it will split into sub-nodes.
+    /// The rectangle to insert into the node.
     public override void Insert(Rectangle rect)
     {
-        if (Rectangles.Count < 5)
+        if (Rectangles.Count < threshold)
         {
             // If the node is not full, insert the rectangle directly
             Rectangles.Add(rect);
         }
         else
         {
-            // If the node is full, we need to split it and redistribute rectangles
-            SplitNode();
-
-            // After splitting, insert the rectangle into the appropriate child node
-            InsertIntoChildNode(rect);
+            // If the node is full, we should notify the parent node to handle the split
+            // This is where you would call the parent node's split function, but we don't do this here
+            Console.WriteLine("Node exceeds threshold, should handle split logic outside of LeafNode.");
         }
     }
 
-    /// <summary>
-    /// [TODO:description]
-    /// </summary>
-    /// <param name="x">[TODO:description]</param>
-    /// <param name="y">[TODO:description]</param>
-    public override void Delete(int x, int y) //NOTE: you should modify this to take a Rectangle parameter instead.
+    /// Deletes a rectangle based on its coordinates from the node.
+    /// The x-coordinate of the rectangle to delete.
+    /// The y-coordinate of the rectangle to delete.
+    public override void Delete(int x, int y)
     {
         var rect = Rectangles.FirstOrDefault(r => r.x == x && r.y == y);
         if (rect != null)
         {
             Rectangles.Remove(rect);
+            Console.WriteLine($"Rectangle at ({x}, {y}) deleted.");
+        }
+        else{
+            Console.WriteLine($"No rectangle found at ({x}, {y}) to delete.");
         }
     }
 
-    /// <summary>
-    /// [TODO:description]
-    /// </summary>
-    /// <param name="level">[TODO:description]</param>
+    public void Delete(Rectangle rect)
+{
+    // Call the existing Delete method
+    Delete(rect.x, rect.y); 
+}
+
+
+
+    /// Dumps the list of rectangles in the current node.
+    ///The current depth level of the node, used for indentation in output.
     public override void Dump(int level)
-    {
+    {Console.WriteLine(new string('\t', level) + $"Leaf Node at [{xMin},{yMin}] to [{xMax},{yMax}]");
         foreach (var rect in Rectangles)
         {
             Console.WriteLine(new string('\t', level) + $"Rectangle at {rect.x}, {rect.y}: {rect.length}x{rect.width}");
         }
     }
 
-    /// <summary>
-    /// [TODO:description]
-    /// </summary>
-    /// <param name="x">[TODO:description]</param>
-    /// <param name="y">[TODO:description]</param>
-    /// <param name="length">[TODO:description]</param>
-    /// <param name="width">[TODO:description]</param>
+    /// Updates the dimensions of a rectangle in the node.
+    /// The x-coordinate of the rectangle to update.
+    /// The y-coordinate of the rectangle to update.
     public override void Update(int x, int y, int length, int width)
     {
-        var rect = Rectangles.FirstOrDefault(r => r.y == x && r.y == y);
+        var rect = Rectangles.FirstOrDefault(r => r.x == x && r.y == y);
         if (rect != null)
         {
+            /// The new length of the rectangle.
             rect.length = length;
+            /// The new width of the rectangle.
             rect.width = width;
+            Console.WriteLine($"Updated rectangle at ({x}, {y}) to {length}x{width}.");
+        }
+        else{
+            Console.WriteLine($"No rectangle found at ({x}, {y}) to update.");
         }
     }
 
-    /// <summary>
-    /// [TODO:description]
-    /// </summary>
-    /// <param name="x">[TODO:description]</param>
-    /// <param name="y">[TODO:description]</param>
-    /// <returns>[TODO:description]</returns>
+/// Finds a rectangle by its coordinates in the current node. 
+/// The x-coordinate of the rectangle to find.
+/// The y-coordinate of the rectangle to find.
     public override Rectangle Find(int x, int y)
     {
+        ///The found rectangle, or null if no rectangle exists at the given coordinates
         return Rectangles.FirstOrDefault(r => r.x == x && r.y == y);
-    }
-
-    /// <summary>
-    /// [TODO:description]
-    /// </summary>
-    private void SplitNode()
-    {
-        // Calculate the midpoint of the current node space
-        int midX = (xMin + xMax) / 2;
-        int midY = (yMin + yMax) / 2;
-
-        // Create four child nodes representing the four quadrants of the current node's space
-        List<Node> children = new List<Node>
-        {
-            new LeafNode(xMin, midY, midX, yMax),  // Top-left
-            new LeafNode(midX, midY, xMax, yMax),  // Top-right
-            new LeafNode(xMin, yMin, midX, midY),  // Bottom-left
-            new LeafNode(midX, yMin, xMax, midY)   // Bottom-right
-        };
-
-        // Redistribute existing rectangles to the child nodes based on their positions
-        foreach (var rect in Rectangles)
-        {
-            InsertIntoChildNode(rect);
-        }
-
-        // Clear the current node's rectangles as they've been moved to the children
-        Rectangles.Clear();
-    }
-
-    private void InsertIntoChildNode(Rectangle rect)
-    {
-        // NOTE: I'm not going to fix or advise on this because leafnodes should not have 
-        // children, and as a result, this method should be deleted.
-
-        /*
-        // Determine which child node (quadrant) the rectangle should go into
-         Node targetChild = null;
-
-        // Check the position of the rectangle and assign it to the correct child node
-        if (rect.xMin < (XMin + XMax) / 2 && rect.YMax >= (YMin + YMax) / 2)
-        {
-            targetChild = children[0];  // Top-left quadrant
-        }
-        else if (rect.XMin >= (XMin + XMax) / 2 && rect.YMax >= (YMin + YMax) / 2)
-        {
-            targetChild = Children[1];  // Top-right quadrant
-        }
-        else if (rect.XMin < (XMin + XMax) / 2 && rect.YMax < (YMin + YMax) / 2)
-        {
-            targe
-        */
     }
 }
