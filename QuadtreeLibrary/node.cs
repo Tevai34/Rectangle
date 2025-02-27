55084a428cd772b70256f9c1a36eb575b5ed3426
Tevai34 <117667970+Tevai34@users.noreply.github.com>
2025-02-27 14:08:25 -0500

    Update node.cs

--- QuadtreeLibrary/node.cs
+++ QuadtreeLibrary/node.cs
@@ -1,41 +1,32 @@
 namespace Quadtree;
 
-/// <summary>
-/// [TODO:description]
-/// </summary>
+/// Represents a node in the quadtree. A node can be either an internal node (with children)
+/// or a leaf node (storing rectangles).
 public abstract class Node
 {
-    //NOTE: you should delete this. Only the leaf node has rectangles.
-    //InternalNodes do not.
-    public List<Rectangle> Rectangles { get; set; }
-
-    /// <summary>
-    /// [TODO:description]
-    /// </summary>
+    /// The minimum x-coordinate of the node's boundary.
     public int xMin { get; set; }
 
-    /// <summary>
-    /// [TODO:description]
-    /// </summary>
+    /// The minimum y-coordinate of the node's boundary.
+
     public int yMin { get; set; }
 
-    /// <summary>
-    /// [TODO:description]
-    /// </summary>
+    /// The maximum x-coordinate of the node's boundary.
     public int xMax { get; set; }
 
-    /// <summary>
-    /// [TODO:description]
-    /// </summary>
-    public int yMax { get; set; }
+    /// The maximum y-coordinate of the node's boundary.
 
-    /// <summary>
-    /// [TODO:description]
-    /// </summary>
-    /// <param name="xMin">[TODO:description]</param>
-    /// <param name="yMin">[TODO:description]</param>
-    /// <param name="xMax">[TODO:description]</param>
-    /// <param name="yMax">[TODO:description]</param>
+    public int yMax { get; set; }
+    /// A list of rectangles contained within this node. This is primarily used by leaf nodes.
+    /// Internal nodes typically delegate storage to their child nodes.
+
+    public List<Rectangle> Rectangles { get; set; }
+
+    /// Initializes a new instance of the class.
+    /// The minimum x-coordinate of the node's boundary.
+    /// The minimum y-coordinate of the node's boundary.
+    /// The maximum x-coordinate of the node's boundary.
+    /// The maximum y-coordinate of the node's boundary.
     public Node(int xMin, int yMin, int xMax, int yMax)
     {
         Rectangles = new List<Rectangle>();
@@ -45,39 +36,28 @@ public abstract class Node
         this.yMax = yMax;
     }
 
-    /// <summary>
-    /// [TODO:description]
-    /// </summary>
-    /// <param name="rect">[TODO:description]</param>
+    /// Inserts a rectangle into the node.
+    /// If the node is a leaf, it stores the rectangle directly.
+    /// If the node is an internal node, it delegates the insertion to the appropriate child.
+    /// The rectangle to insert.
     public abstract void Insert(Rectangle rect);
 
-    /// <summary>
-    /// [TODO:description]
-    /// </summary>
-    /// <param name="x">[TODO:description]</param>
-    /// <param name="y">[TODO:description]</param>
+    /// Deletes a rectangle from the node based on the given coordinates.
+    /// The x-coordinate of the rectangle to delete.
+    /// The y-coordinate of the rectangle to delete.
     public abstract void Delete(int x, int y);
 
-    /// <summary>
-    /// [TODO:description]
-    /// </summary>
-    /// <param name="x">[TODO:description]</param>
-    /// <param name="y">[TODO:description]</param>
-    /// <returns>[TODO:description]</returns>
+    /// Finds a rectangle in the quadtree based on the given coordinates.
+    /// The x-coordinate of the rectangle to find.
+    /// The y-coordinate of the rectangle to find.
     public abstract Rectangle Find(int x, int y);
 
-    /// <summary>
-    /// [TODO:description]
-    /// </summary>
-    /// <param name="x">[TODO:description]</param>
-    /// <param name="y">[TODO:description]</param>
-    /// <param name="length">[TODO:description]</param>
-    /// <param name="width">[TODO:description]</param>
+    /// Updates a rectangle's dimensions in the node.
+    /// The x-coordinate of the rectangle to update.
+    /// The y-coordinate of the rectangle to update.
+    /// The new length of the rectangle.
+    /// The new width of the rectangle.
     public abstract void Update(int x, int y, int length, int width);
 
-    /// <summary>
-    /// [TODO:description]
-    /// </summary>
-    /// <param name="level">[TODO:description]</param>
+    /// The depth level of the current node used for indentation.
     public abstract void Dump(int level);
-}
