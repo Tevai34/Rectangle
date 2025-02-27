a1f0f6ddde71ec2c83adb5382f3fca8f2697a9ef
Tevai34 <117667970+Tevai34@users.noreply.github.com>
2025-02-27 14:05:08 -0500

    Update Rectangle.cs

--- QuadtreeLibrary/Rectangle.cs
+++ QuadtreeLibrary/Rectangle.cs
@@ -1,45 +1,56 @@
-namespace Quadtree;
-
-/// <summary>
-/// [TODO:description]
-/// </summary>
-public class Rectangle
+namespace Quadtree
 {
-    /// <summary>
-    /// [TODO:description]
-    /// </summary>
-    public int x { get; set; }
-
-    /// <summary>
-    /// [TODO:description]
-    /// </summary>
-    public int y { get; set; }
-
-    /// <summary>
-    /// [TODO:description]
-    /// </summary>
-    public int length { get; set; }
-
-    /// <summary>
-    /// [TODO:description]
-    /// </summary>
-    public int width { get; set; }
-
-    /// <summary>
-    /// [TODO:description]
-    /// </summary>
-    /// <param name="x">[TODO:description]</param>
-    /// <param name="y">[TODO:description]</param>
-    /// <param name="length">[TODO:description]</param>
-    /// <param name="width">[TODO:description]</param>
-    public Rectangle(int x, int y, int length, int width)
+    /// Represents a rectangle defined by its position and dimensions.
+    public class Rectangle
     {
-        // NOTE: variables should begin with a lowercase letter.
-        // You can access variable of the class with the same 
-        // name as parameters using: this.<variable name>
-        this.x = x;
-        this.y = y;
-        this.length = length;
-        this.width = width;
+        /// Gets or sets the x-coordinate of the rectangle's top-left corner.
+        public int x { get; set; }
+
+        /// Gets or sets the y-coordinate of the rectangle's top-left corner.
+        public int y { get; set; }
+
+        /// Gets or sets the length (height) of the rectangle.
+        public int length { get; set; }
+
+        /// <summary>
+        /// Gets or sets the width of the rectangle.
+        /// </summary>
+        public int width { get; set; }
+
+        /// Initializes a new instance of the class with specified coordinates and dimensions.
+        /// The x-coordinate of the rectangle's top-left corner.
+        /// The y-coordinate of the rectangle's top-left corner.
+        /// The length (height) of the rectangle.
+        /// The width of the rectangle.
+        public Rectangle(int x, int y, int length, int width)
+        {
+            this.x = x;
+            this.y = y;
+            this.length = length;
+            this.width = width;
+        }
+
+        /// Gets the area of the rectangle (length * width).
+        /// The area of the rectangle.
+        public int GetArea()
+        {
+            return length * width;
+        }
+
+        /// Gets the perimeter of the rectangle (2 * (length + width)).
+        /// The perimeter of the rectangle.
+        public int GetPerimeter()
+        {
+            return 2 * (length + width);
+        }
+
+        /// Determines if a given point (px, py) is inside the rectangle.
+        /// The x-coordinate of the point to check.
+        /// The y-coordinate of the point to check.
+        ///True if the point is inside the rectangle, otherwise false.
+        public bool IsPointInside(int px, int py)
+        {
+            return px >= x && px <= x + width && py >= y && py <= y + length;
+        }
     }
 }
